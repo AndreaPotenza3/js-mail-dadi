a3c5f9d530258da126e44d1623bef0aea68d07e3
AndreaPotenza <andreapoten3@gmail.com>
2024-10-18 17:41:23 +0200

    esercizio emails

---
+++ js/app.js
@@ -0,0 +1,33 @@
+const mails = ['pincopallo@gmal.com', 'pallino@gmail.com', 'carletto@gmail.com', 'faker@gmail.com']
+
+// chedere all'utente la sua mal
+
+const userMail = prompt('Inserisci la tua email')
+
+// controllare che sa nella lista
+
+let correctMail = false
+
+for (let i = 0; i < mails.length; i++) {
+    
+
+    const  currentMail = mails[i]
+    console.log(currentMail === userMail)
+
+    if(currentMail === userMail){
+    correctMail = true
+    break
+    }
+    
+}
+
+console.log(correctMail)
+
+//stampa messaggo
+
+if(correctMail) {
+    console.log('Benvenuto nella pagina')
+}else {
+    console.log('Accesso negato')
+}
+
